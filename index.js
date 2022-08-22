9b29710259b25c1cd53399f0b71b5bfcff92a21d
chris <chrishethertonweb@gmail.com>
2022-08-22 15:37:58 +0200

    all done

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function sum(insertedArray) {
   if (insertedArray.length === 0) {
     return 0
   }
-  console.log(insertedArray);
+  // console.log(insertedArray);
   return insertedArray.pop() + sum(array)
 
 
 }
 
 const array = [1, 2, 3, 4, 5, 6]
-console.log(sum(array));  // return 21
+sum(array);  // return 21
